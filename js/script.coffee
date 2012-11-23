98fc9b662c078063074f420e931d0d7db0c0ea65
Jonathan Breizh <jonbreizh@gmail.com>
2012-11-23 17:05:41 +0100

    working prototype
    All 3 examples are set, including the dynamic one
    Discovered you could only use at the moment either filter:custom() OR
    css 3d native properties
    Interesting demo for edge bleeding css lovers but would need a lil UI
    love though

--- js/script.coffee
+++ js/script.coffee
@@ -6,33 +6,46 @@ verteX = {
 	areaPos : $('.holder.big').position(),
 	areaMX : $('.mouseX'),
 	areaMY : $('.mouseY'),	
+	movingArea : $('.broken_square.user'),
 	HorVal : $('.HOR'),
 	VerVal : $('.VER'),
-	minHor : 30,
-	maxHor : -30,
-	minVer : 20,
-	maxVer : -20,
+	minHor : 60,
+	maxHor : -60,
+	minVer : 30,
+	maxVer : -30,
+	curentX : 0,
+	curentY : 0,
+	transform : 'rotateX('+0+'deg) rotateY('+0+'deg)',
+	filter : 'custom(url(shaders/detached_tiles.vs) mix(url(shaders/detached_tiles.fs) normal source-atop), 5 5 border-box detached, amount 75.0, t 0.0, transform rotateX(0deg) scale(0.9)) grayscale(0)',
 
 	init : ->
 		verteX.stepsCalcul();
+		console.log(document.styleSheets[0].cssRules);
 		true
 
 	stepsCalcul : ->
 		divWidth = parseInt(verteX.area.css('width'))
 		divHeight = parseInt(verteX.area.css('height'))
-		verteX.HorStep = divWidth/(parseInt(verteX.minHor)+parseInt(verteX.maxHor))
-		verteX.VerStep = divHeight/(parseInt(verteX.minVer)+parseInt(verteX.maxVer))
+		verteX.HorStep = divWidth/(verteX.minHor+Math.abs(verteX.maxHor))
+		verteX.VerStep = divHeight/(verteX.minVer+Math.abs(verteX.maxVer))
 		verteX.mouseCoordinates();
 		true
 
 
 	mouseCoordinates : ->
-		$('.holder.big').mousemove((e)->
-			verteX.areaMX.text(e.pageX-parseInt(verteX.areaPos.left))
-			verteX.areaMY.text(e.pageY-parseInt(verteX.areaPos.top))
-			verteX.HorVal.text(verteX.minHor-((e.pageX-parseInt(verteX.areaPos.left))/verteX.HorStep))
+		verteX.area.mousemove((e)->
+			verteX.curentX = (verteX.minHor-((e.pageX-parseInt(verteX.areaPos.left))/verteX.HorStep)).toFixed(1)
+			verteX.curentY = (verteX.minVer-((e.pageY-parseInt(verteX.areaPos.top))/verteX.VerStep)).toFixed(1)
+			#At the time of this demo, it semms unlikely to mix native css3d properties with filter:custom() one, it's either one or the other on the same dom element
+			#verteX.transform = 'rotateX('+verteX.curentY+'deg) rotateY('+verteX.curentX+'deg)'
+			math = (Math.abs(verteX.curentX*0.01))+0.2
+			#console.log math
+			num = Math.abs(verteX.curentY).toFixed(0)
+			verteX.filter = 'custom(url(shaders/detached_tiles.vs) mix(url(shaders/detached_tiles.fs) normal source-atop), '+num+' 5 border-box detached, amount '+(verteX.curentY)*5+', t 1.0, transform rotateX('+(verteX.curentY*0.5)+'deg) rotateZ('+verteX.curentY+'deg) rotateY('+verteX.curentX+'deg) scale('+math+')) grayscale(0) drop-shadow(1px 1px 1px black)'
+			verteX.movingArea.css('-webkit-filter',verteX.filter)
 			true
 		)
+		true
 
 #fin de l'objet
 }
--- js/script.js
+++ js/script.js
@@ -7,34 +7,43 @@
     areaPos: $('.holder.big').position(),
     areaMX: $('.mouseX'),
     areaMY: $('.mouseY'),
+    movingArea: $('.broken_square.user'),
     HorVal: $('.HOR'),
     VerVal: $('.VER'),
-    minHor: 30,
-    maxHor: -30,
-    minVer: 20,
-    maxVer: -20,
+    minHor: 60,
+    maxHor: -60,
+    minVer: 30,
+    maxVer: -30,
+    curentX: 0,
+    curentY: 0,
+    transform: 'rotateX(' + 0 + 'deg) rotateY(' + 0 + 'deg)',
+    filter: 'custom(url(shaders/detached_tiles.vs) mix(url(shaders/detached_tiles.fs) normal source-atop), 5 5 border-box detached, amount 75.0, t 0.0, transform rotateX(0deg) scale(0.9)) grayscale(0)',
     init: function() {
       verteX.stepsCalcul();
+      console.log(document.styleSheets[0].cssRules);
       return true;
     },
     stepsCalcul: function() {
       var divHeight, divWidth;
       divWidth = parseInt(verteX.area.css('width'));
       divHeight = parseInt(verteX.area.css('height'));
-      verteX.HorStep = divWidth / ((Math.abs(verteX.minHor) + Math.abs(verteX.maxHor)));
-      verteX.VerStep = divHeight / ((Math.abs(verteX.minVer) + Math.abs(verteX.maxVer)));
-        console.log(verteX.minHor);
-        console.log(verteX.maxHor);
+      verteX.HorStep = divWidth / (verteX.minHor + Math.abs(verteX.maxHor));
+      verteX.VerStep = divHeight / (verteX.minVer + Math.abs(verteX.maxVer));
       verteX.mouseCoordinates();
       return true;
     },
     mouseCoordinates: function() {
-      return $('.holder.big').mousemove(function(e) {
-        verteX.areaMX.text(e.pageX - parseInt(verteX.areaPos.left));
-        verteX.areaMY.text(e.pageY - parseInt(verteX.areaPos.top));
-        verteX.HorVal.text(verteX.minHor - ((e.pageX - parseInt(verteX.areaPos.left)) / verteX.HorStep));
+      verteX.area.mousemove(function(e) {
+        var math, num;
+        verteX.curentX = (verteX.minHor - ((e.pageX - parseInt(verteX.areaPos.left)) / verteX.HorStep)).toFixed(1);
+        verteX.curentY = (verteX.minVer - ((e.pageY - parseInt(verteX.areaPos.top)) / verteX.VerStep)).toFixed(1);
+        math = (Math.abs(verteX.curentX * 0.01)) + 0.2;
+        num = Math.abs(verteX.curentY).toFixed(0);
+        verteX.filter = 'custom(url(shaders/detached_tiles.vs) mix(url(shaders/detached_tiles.fs) normal source-atop), ' + num + ' 5 border-box detached, amount ' + verteX.curentY * 5 + ', t 1.0, transform rotateX(' + (verteX.curentY * 0.5) + 'deg) rotateZ(' + verteX.curentY + 'deg) rotateY(' + verteX.curentX + 'deg) scale(' + math + ')) grayscale(0) drop-shadow(1px 1px 1px black)';
+        verteX.movingArea.css('-webkit-filter', verteX.filter);
         return true;
       });
+      return true;
     }
   };
 
